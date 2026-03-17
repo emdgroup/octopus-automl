f7910cc9172833231b75b33e80d32f109a015a99
kalama-ai <kathrin.skubch@merckgroup.com>
2026-03-17 10:39:40 +0100

    Replace literal field in MRMR module

--- octopus/modules/mrmr/module.py
+++ octopus/modules/mrmr/module.py
@@ -7,7 +7,7 @@ from typing import Literal
 from attrs import Factory, define, field, validators
 
 from ..base import ModuleExecution, Task
-
+from octopus.types import FIComputeMethod
 
 @define
 class Mrmr(Task):
@@ -50,9 +50,12 @@ class Mrmr(Task):
     )
     """Selection of feature importance type."""
 
-    feature_importance_method: Literal["permutation", "shap", "internal", "lofo"] = field(
-        validator=validators.in_(["permutation", "shap", "internal", "lofo"]), default="permutation"
-    )
+    feature_importance_method: FIComputeMethod = field(
+    converter=FIComputeMethod,
+    validator=validators.in_([FIComputeMethod.PERMUTATION, FIComputeMethod.SHAP,
+                               FIComputeMethod.INTERNAL, FIComputeMethod.LOFO]),
+    default=FIComputeMethod.PERMUTATION,
+)
     """Selection of feature importance method."""
 
     def create_module(self) -> ModuleExecution:
